f5c0b1ffaca40b42c121d780012717e84906d273
Cade Forrest Roberts <cadefroberts@gmail.com>
2021-01-26 19:48:13 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,24 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+function whisper(string) {
+  return string.toLowerCase()
+}
+function logShout(string) {
+  console.log("HELLO");
+
+}
+function logWhisper(string) {
+  console.log("hello");
+
+}
+function sayHiToGrandma(hello) {
+    return ("I can't hear you!")
+
+  }
+function sayHiToGrandma(Hello){
+    return ("YES INDEED!")
+  }
+function sayHiToGrandma("I love you, Grandma"){
+  return ("I love you, too.")
+}
